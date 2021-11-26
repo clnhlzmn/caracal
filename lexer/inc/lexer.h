ace815fa9477cd650ac4f5732967218aa69f3160
Colin Holzman <colin.holzman@protonmail.com>
2021-11-26 14:15:24 -0800

    docs

--- lexer/inc/lexer.h
+++ lexer/inc/lexer.h
@@ -27,8 +27,21 @@ struct lexer_token {
     lexer_token *next;
 };
 
+/**
+ * @brief Analyzes input and produces a list of tokens.
+ * The output token list is dynamically allocated by @ref
+ * lexer_lex and should be freed by the caller.
+ * @param [out] output Pointer to the ouput token list.
+ * @param [in] input Pointer to the input character string.
+ * @return Returns LEXER_SUCCESS if successful.
+ */
 lexer_return lexer_lex(lexer_token **output, const char *input);
 
+/**
+ * @brief Frees a token list that was produced by @ref lexer_lex.
+ * @param [in] tokens Pointer to the token list.
+ * @return Returns LEXER_SUCCESS if successful.
+ */
 lexer_return lexer_free(lexer_token *tokens);
 
 #endif /*LEXER_H*/
