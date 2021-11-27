d07fc707937561185d147cd5ce8a456d13ff272d
Colin Holzman <colin.holzman@protonmail.com>
2021-11-27 09:21:16 -0800

    Add check on err in lexer_line_and_column_test

--- lexer/test/lexer_line_and_column_test.c
+++ lexer/test/lexer_line_and_column_test.c
@@ -6,6 +6,7 @@ int main(void) {
     lexer_token *output;
     const char *input = "foo\nbar\r\n baz";
     lexer_return err = lexer_lex(&output, input);
+    assert(err == LEXER_SUCCESS);
     assert(output->line == 1 && output->column == 1);
     output = output->next;
     assert(output->line == 2 && output->column == 1);
