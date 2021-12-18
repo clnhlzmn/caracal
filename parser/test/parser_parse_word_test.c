724bf45566e307fbed58f235f317de117e9e106e
Colin <colin.holzman@protonmail.com>
2021-12-17 22:16:04 -0800

    Fix error in parser_parse_word.

--- parser/test/parser_parse_word_test.c
+++ parser/test/parser_parse_word_test.c
@@ -11,9 +11,13 @@ int main(void) {
     assert(parser_parse_word(tokens).error == PARSER_SUCCESS && "() should parse successfully");
     lexer_lex(&tokens, "(foo 123)");
     assert(parser_parse_word(tokens).error == PARSER_SUCCESS && "(foo 123) should parse successfully");
+    lexer_lex(&tokens, "((foo) bar)");
+    assert(parser_parse_word(tokens).error == PARSER_SUCCESS && "((foo) bar) should parse successfully");
     lexer_lex(&tokens, "(");
     assert(parser_parse_word(tokens).error == PARSER_FAILURE && "( should fail to parse");
     lexer_lex(&tokens, "(foo");
     assert(parser_parse_word(tokens).error == PARSER_FAILURE && "(foo should fail to parse");
+    lexer_lex(&tokens, "((foo)");
+    assert(parser_parse_word(tokens).error == PARSER_FAILURE && "((foo) should fail to parse");
     return 0;
 }
