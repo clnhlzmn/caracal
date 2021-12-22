176eb47de68b89347db6d89426aa2c490066fc3c
Colin Holzman <colin.holzman@protonmail.com>
2021-12-21 21:24:45 -0800

    Add cases to parser_parse_file_test.

--- parser/test/parser_parse_file_test.c
+++ parser/test/parser_parse_file_test.c
@@ -1,4 +1,5 @@
 #include "parser.h"
+#include "source_file.h"
 #include <assert.h>
 
 int main(void) {
@@ -7,5 +8,22 @@ int main(void) {
     parser_return result = parser_parse_file(tokens, "foo");
     assert(!result.error);
     assert(result.value);
+    assert(source_file_equal(result.value, source_file_make("foo", NULL)));
+    
+    lexer_lex(&tokens, "let foo = 123 .\nlet bar = 456 .");
+    result = parser_parse_file(tokens, "foo");
+    assert(!result.error);
+    assert(result.value);
+    assert(
+        source_file_equal(
+            result.value, 
+            source_file_make("foo", 
+                def_make_list(2, 
+                    def_make_let("foo", word_make_quote(word_make_number(123))), 
+                    def_make_let("bar", word_make_quote(word_make_number(456)))
+                )
+            )
+        )
+    );
     return 0;
 }
