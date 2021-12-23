65322064ff7d57c4da528a9a2485c50a0a4203ea
Colin <colin.holzman@protonmail.com>
2021-12-22 19:20:05 -0800

    More vm things.

--- vm/inc/vm.h
+++ vm/inc/vm.h
@@ -1,4 +1,19 @@
 #ifndef VM_H
 #define VM_H
 
+#define IS_INT(v) (v & 1)
+#define MARK_INT(v) ((v << 1) | 1)
+#define INT_VALUE(v) (v >> 1)
+
+typedef struct {
+    intptr_t first;
+    intptr_t second;
+} pair;
+
+typedef struct {
+    pair *memory;
+    size_t heap_size;
+    pair *free_list;
+} heap;
+
 #endif /* VM_H */
